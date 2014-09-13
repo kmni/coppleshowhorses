d15bd57bc165b452b1b8a95645b4a802e4adaaa0
Tomas Musiol <tomas.musiol@gmail.com>
2014-09-13 08:11:59 +0200

    Slider at homepage

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,2 +1,24 @@
 $(document).ready(function() {
+
+if( $(".js-carousel").length > 0 ){
+    $('.js-carousel').jcarousel({
+        wrap: 'circular'
+    });
+    $('.js-carousel').jcarouselAutoscroll({
+        interval: 5000
+    });
+    $('.js-carousel-pager')
+    .on('jcarouselpagination:active', 'a', function() {
+        $(this).addClass('is-active');
+    })
+    .on('jcarouselpagination:inactive', 'a', function() {
+        $(this).removeClass('is-active');
+    })
+    .jcarouselPagination({
+        item: function(page) {
+            return '<a class="slider-pager__item" href="#' + page + '">' + page + '</a>';
+        }
+    });
+}
+
 });
