56770afab45c1fd3319a80ea586485af7bcd79a2
Tomas Musiol <tomas.musiol@gmail.com>
2014-10-01 14:11:05 +0200

    Responsive menu

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+// mobile menu
+// ====================================
+
+$(".mobile_header__handler").click(function(){
+    $(".mobile_menu").toggle();
+    return false;
+});
+
+
 // fancybox
 // ====================================
 
